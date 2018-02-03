e7cf1a36a30ad5a8edd668ce032bf3553d2357ba
bricktrooper <kylepinto@yahoo.ca>
2018-02-02 21:56:33 -0500

    Mecanum

--- src/org/usfirst/frc/team4015/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team4015/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team4015.robot.RobotMap;
 import org.usfirst.frc.team4015.robot.commands.Drive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /* ===================================================
@@ -20,6 +21,7 @@ public class Drivetrain extends Subsystem
 	public Talon backRight;
 	
 	public RobotDrive chassis;
+	//public MecanumDrive chassis;
 	
 	// DEFAULT CONSTRUCTOR //
 	
@@ -45,7 +47,7 @@ public class Drivetrain extends Subsystem
 		frontRight = new Talon(RobotMap.frontRightMotor);
 		backLeft = new Talon(RobotMap.backLeftMotor);
 		backRight = new Talon(RobotMap.backRightMotor);
-		chassis = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+		//chassis = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 	}
 	
 	/* =========== TANK DRIVE ===========
@@ -58,14 +60,40 @@ public class Drivetrain extends Subsystem
 		chassis.arcadeDrive(throttle, yaw);
 	}
 	
-	// TODO Mecanum Drive //
-	
 	// STOP //
 	
 	public void stop()
 	{
 		tankDrive(0, 0);
 	}
+	
+	// MECANUM //
+	
+	/* ======================================
+	 *                DRIVE
+	 * ======================================                
+	 * ------------MECANUM DRIVE-------------
+	 * xSpeed ---> 1.0 = 100% right
+	 * xSpeed ---> -1.0 = 100% left
+	 * ySpeed ---> 1.0 = 100% forward
+	 * ySpeed ---> -1.0 = 100% backward
+	 * rotation ---> 1.0 = 100% CW
+	 * rotation ---> -1.0 = 100% CCW
+	 * 
+	 *---------------TANK DRIVE-------------
+	 * Same as mecanum but make xSpeed = 0.0
+	 * =====================================*/
+	/*
+	public void drive(double xSpeed, double ySpeed, double rotation)
+	{
+		chassis.driveCartesian(xSpeed, ySpeed, rotation);
+	}
+	
+	public void stop()
+	{
+		drive(0, 0, 0);
+	}
+	*/
 
 	public void initDefaultCommand()
 	{
